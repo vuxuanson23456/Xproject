cc6de272575515ebb831625bc2d786566e645007
vuxuanson23456 <140165675+vuxuanson23456@users.noreply.github.com>
2023-08-04 17:38:38 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,75 +1,15 @@
 <?php
-    require "condb.php";
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    
-    <style>
-        tr, td, th {
-            border: 1px solid black;
-            padding:20px;
-            background-color: #
-        }
+$controller = isset($_GET['c']) ? $_GET['c'] : 'user'; //Nếu c tồn tại > Lấy giá trị c còn ko c=home
+$action = isset($_GET['a']) ? $_GET['a'] : 'index'; ////Nếu a tồn tại > Lấy giá trị a còn ko c=index
+//2. Gọi ra được cái tệp chứa định nghĩa Controller >>> Gọi hàm sẽ xử lý nhiệm vụ tiếp theo.
+//home >>> HomeController.php
+$controller = ucfirst($controller);
+$controller = $controller . "Controllers";
+$pathController = "app/controllers/" . $controller . ".php";
 
-        .header{
-            display: flex;
-            align-items: center;
-            
-        }
-
-        .add{
-            display: flex;
-            align-items: center;
-            width: 200px;
-            height: 30px; 
-            background-color: #28A745; 
-            margin-left:120px;
-            justify-content:center;
-        }   
-        
-    </style>
-</head>
-<body>
-    <div class="header">
-        <h1 style="color:#1C7FC5">LIST OF EMPLOYEES</h1>
-        <div class="add"><a href="add.php">Add New Employee</a></div>
-    </div>
-    <div class="container">
-        <table >
-            <tr>
-                <!-- <th>stt</th> -->
-                <th>Stt</th>
-                <th>Name</th>
-                <th>Address</th>
-                <th>salary</th>
-                <th>Action</th>
-            </tr>
-            <?php
-            
-                $sql ="SELECT * FROM list";
-                $result = $conn->query($sql);
-                $stt = 1;
-                while($row = $result->fetch_assoc()) {
-            ?>
-                    
-                    <tr>
-                        <td><?php echo $stt++; ?></td>
-                        <td><?php echo $row['name'] ?></td>
-                        <td><?php echo $row['address'] ?></td>
-                        <td><?php echo $row['salary'] ?></td>
-                        <td> 
-                            <a href="fix.php?id=<?php echo $row['id']; ?>">Sửa</a> | <a href="delete.php?id=<?php echo $row['id']; ?>">Xóa</a></td>
-                        
-                    </tr>
-            <?php
-                }
-            ?>
-            
-        </table>
-    </div>
-</body>
-</html>
+if (!file_exists($pathController)) {
+	die('Yêu cầu Controller không tồn tại, vui lòng kiểm tra lại liên kết');
+}
+include($pathController);
+$myController = new $controller();
+$myController->$action();
